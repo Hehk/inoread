926c88e19983daf06772d56d4c53e00ac28bccf8
Kyle Henderson <ona@lunch.run>
2023-08-06 13:49:47 -0500

    Cleanup copy

--- bin/home_page.ml
+++ bin/home_page.ml
@@ -1,14 +1,16 @@
 let about =
   [
-    "Hoi! I am a software engineer based in Austin, TX. I am current a \
-     founding engineer at Pineway and previous an engineer at Superhuman.";
-    "Right now I am writing go and typescript in the streets and ocaml in the \
-     sheets.";
+    "Hoi! I am currently a founding engineer at Throne and I was previously an \
+     engineer at Superhuman.";
+    "Right now, I am writing a bunch of Swift for iOS and Python to train \
+     machine learning models to pay the bills. Outside of work, I write mostly \
+     OCaml to do anything that looks interesting";
+    "While touching grass, I am trying to move from conversation to fluent in \
+     Dutch, learning the piano, and running enough that I am always a little \
+     tired.";
   ]
 
 let content () =
   let open Tyxml.Html in
-  let about =
-    about |> List.map (fun x -> Template.Tag.p [ txt x ])
-  in
+  let about = about |> List.map (fun x -> Template.Tag.p [ txt x ]) in
   Template.centered @@ about @ Projects_page.active_projects_section
